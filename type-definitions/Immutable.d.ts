5a78e62dbf88dfd62eef2a1c37748db289456596
Julien Deniau <julien.deniau@gmail.com>
2021-07-02 14:33:51 +0200

    working version of ObjectLikeMap with `.get()`

--- type-definitions/Immutable.d.ts
+++ type-definitions/Immutable.d.ts
@@ -764,16 +764,24 @@ declare module Immutable {
   export function Map(): Map<unknown, unknown>;
   export function Map<K, V>(): Map<K, V>;
   export function Map<K, V>(collection: Iterable<[K, V]>): Map<K, V>;
-  export function Map<
-    R extends { [key in string | number]: unknown },
-    K extends keyof R,
-    V extends R[K]
-  >(obj: R): ObjectLikeMap<R, K, V>;
+  export function Map<R extends { [key in string | number]: unknown }>(
+    obj: R
+  ): ObjectLikeMap<R>;
   // export function Map<V>(obj: { [key: string]: V }): Map<string, V>;
 
-  export interface ObjectLikeMap<R extends {[key in string|number]: unknown}, K extends keyof R, V extends R[K]> extends Map<K, V> {
-    get(index: K, notSetValue: never): R[K];
-    get(index: K): R[K];
+  export interface ObjectLikeMap<
+    R extends { [key in string | number]: unknown }
+  > extends Map<keyof R, R[keyof R]> {
+    /**
+     * Returns the value associated with the provided key, or notSetValue if
+     * the Collection does not contain this key.
+     *
+     * Note: it is possible a key may be associated with an `undefined` value,
+     * so if `notSetValue` is not provided and this method returns `undefined`,
+     * that does not guarantee the key was not found.
+     */
+    get<K extends keyof R>(key: K, notSetValue?: unknown): R[K];
+    get<NSV>(key: string, notSetValue: NSV): NSV;
   }
 
   export interface Map<K, V> extends Collection.Keyed<K, V> {
@@ -2487,10 +2495,9 @@ declare module Immutable {
     export interface Factory<TProps extends Object> {
       (values?: Partial<TProps> | Iterable<[string, unknown]>): Record<TProps> &
         Readonly<TProps>;
-      new (values?: Partial<TProps> | Iterable<[string, unknown]>): Record<
-        TProps
-      > &
-        Readonly<TProps>;
+      new (
+        values?: Partial<TProps> | Iterable<[string, unknown]>
+      ): Record<TProps> & Readonly<TProps>;
 
       /**
        * The name provided to `Record(values, name)` can be accessed with
